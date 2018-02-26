d3d8cac26d371089f6afad06ad59c5ecee231052
Saikalyan <saikalyan2703@gmail.com>
2018-02-26 18:33:41 -0500

    Add files via upload

---
+++ PalindromeNumber.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPalindrome(int n) {
+        if(n < 0) return false;
+       int divisor = 1;
+        while (n / divisor >= 10)
+            divisor *= 10;
+      
+        while (n != 0)
+        {
+            int leading = n / divisor; 
+            int trailing = n % 10;
+                  
+            if (leading != trailing)  
+                return false;
+            
+            n = (n % divisor) / 10;
+            
+            divisor = divisor / 100;
+        }
+        return true;
+    }
+}
